3f8ead8cd57ed2663f87684a55906028696e61d1
Oskar Potaczek <Amoroth@users.noreply.github.com>
2025-05-31 16:48:45 +0000

    Refactor collect_arguments to accept a Configurable trait and update main to use Arguments struct

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,26 @@
 mod cli_parser;
 
-use cli_parser::collect_arguments;
+use std::{collections::HashMap};
+use cli_parser::{collect_arguments, Configurable};
+
+#[derive(Debug)]
+struct Arguments {
+    pub name: String,
+}
+
+impl Configurable for Arguments {
+    fn populate(&mut self, args: &HashMap<String, String>) {
+        if let Some(name) = args.get("name") {
+            self.name = name.clone();
+        }
+    }
+}
 
 fn main() {
-    let arguments = collect_arguments();
+    let mut arguments = Arguments {
+        name: String::new(),
+    };
+    collect_arguments(&mut arguments);
 
     println!("{:?}", arguments);
 }
