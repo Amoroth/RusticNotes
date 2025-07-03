939da2371265969411bef18e3c0dd54f75b880ae
Oskar Potaczek <Amoroth@users.noreply.github.com>
2025-07-03 10:32:39 +0000

    rewrite arguments collection

--- src/cli_parser.rs
+++ src/cli_parser.rs
@@ -42,26 +42,57 @@ pub trait CliConfigurable {
 }
 
 pub fn collect_arguments<T: CliConfigurable>(config: &mut T) {
-    let arguments = env::args();
-    let mut args_hashmap = HashMap::new();
-    let mut previous_argument_key: Option<String> = None;
+    // let arguments = env::args();
+    // let mut args_hashmap = HashMap::new();
+    // let mut previous_argument_key: Option<String> = None;
 
-    for arg in arguments.skip(1) {
-        if let Some(prev_key) = previous_argument_key.take() {
-            args_hashmap.insert(prev_key, arg);
-        } else if let Some((key, value)) = arg.split_once('=') {
-            args_hashmap.insert(key.to_string(), value.to_string());
+    // for arg in arguments.skip(1) {
+    //     if let Some(prev_key) = previous_argument_key.take() {
+    //         args_hashmap.insert(prev_key, arg);
+    //     } else if let Some((key, value)) = arg.split_once('=') {
+    //         args_hashmap.insert(key.to_string(), value.to_string());
+    //     } else {
+    //         previous_argument_key = Some(arg);
+    //     }
+    // }
+
+    let env_args = env::args();
+    let mut args: Vec<(String, Option<String>)> = vec![];
+    let arugment_definitions = config.get_definitions();
+    let mut previous_argument_definition: Option<&CliArgumentSpecification> = None;
+
+    for arg in env_args.skip(1) {
+        let argument_definition = if arg.starts_with("-") {
+            None
+        } else if arg.starts_with("--") {
+            arugment_definitions.iter().find(|&x| x.name == arg)
         } else {
-            previous_argument_key = Some(arg);
+            if previous_argument_definition.is_some() && !previous_argument_definition.unwrap().is_flag {
+                args.last_mut().unwrap().1 = Some(arg.clone());
+            }
+
+            None
+        };
+
+        if argument_definition.is_some() {
+            args.push((arg.clone(), None));
         }
+
+        previous_argument_definition = argument_definition;
     }
 
-    // collect it all into the vector
+    // x collect it all into the vector
     // the get_definitions method is used to check if the argument is positional or an option
     // check if the key is with a dash, if it is, add it to the vector as a tuple (key, None)
     // if it is not, add it to the vector as a tuple (key, Some(value))
     // take last value in the vector in populate method
     // this will also allow to have multiple values for the same key
 
+    // don't bother with multiple values for now
+    let mut args_hashmap: HashMap<String, String> = HashMap::new();
+    for arg in args {
+        args_hashmap.insert(arg.0, arg.1.unwrap_or_default());
+    }
+    
     config.populate(&args_hashmap);
 }
